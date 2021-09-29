6c8eb5c8b1870d124b71b050232dd4c28a756f2d
seongjae-Leee <89692626+seongjae-Leee@users.noreply.github.com>
2021-09-29 15:01:42 +0900

    Delete join_sj.js
    miss

--- join_sj.js
+++
@@ -1,8 +0,0 @@
-const Sequelize =require('sequelize');
-
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
-const db = {};
-
-const sequelize =new Sequelize(config.database,config.username,configfig.password,config);
-
