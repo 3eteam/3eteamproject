128ddbf985e48a1aa7f7c7f6645c7f738cfce301
seongjae-Leee <89692626+seongjae-Leee@users.noreply.github.com>
2021-09-29 15:04:09 +0900

    Update app.js
    20210929_lsj_login

--- 3e/app.js
+++ 3e/app.js
@@ -4,37 +4,30 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const nunjucks=require('nunjucks');
+const Sequelize =require('sequelize');
 const dotenv=require('dotenv');
-
+const passport = require('passport');
 
 
 
 
 dotenv.config();
 const pageRouter=require('./routes/page');
-const postRouter=require('./routes/post');
 const {sequelize} = require('./models');
+const passportConfig = require('./passport');
 
 const app=express();
+passportConfig();
 app.set('port',process.env.PORT || 8001);
 app.set('view engine','html');
 nunjucks.configure('views',{
     express:app,
     watch:true,
 });
-
-sequelize.sync({force:false})
-.then(()=>{
-    console.log('데이터베이스 연결 성공');
-})
-.catch((err)=>{
-    console.error(err);
-});
 //라우터
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,'public')));
-app.use('img',express.static(path.join(__dirname,'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -48,8 +41,10 @@ app.use(session({
     }
 }));
 
-app.use('/',pageRouter);
-app.use('/post',postRouter);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', pageRouter);
 
 
 app.use((req,res,next)=>{
