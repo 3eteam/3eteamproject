87b33d32dc05e10a695d04398b7623baa9d81136
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-10-01 23:40:57 +0900

    headerm

--- 3e/routes/catlist.js
+++ 3e/routes/catlist.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res, next) => {
       },
       order: [['createdAt', 'DESC']],
     });
-    res.render('layout', {
+    res.render('catlist', {
       title: '3e',
       twits: posts,
     });
