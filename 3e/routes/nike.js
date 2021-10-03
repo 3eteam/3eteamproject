3ca85fc3077a1c2274fa9baf63f667c6ebbdab94
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-10-03 22:18:12 +0900

    nike

--- 3e/routes/nike.js
+++ 3e/routes/nike.js
@@ -26,7 +26,7 @@ router.get('/',  async (req, res, next) => {
   });
   res.render('nike', {
     title: '3e',
-    twits: posts,
+    caps: posts,
   });
   } catch (error) {
     console.error(error);
@@ -57,26 +57,26 @@ router.get('/',  async (req, res, next) => {
 //   }
 // });
 
-router.get('/hashtag', async (req, res, next) => {
-  const query = req.query.hashtag;
-  if (!query) {
-    return res.redirect('/');
-  }
-  try {
-    const hashtag = await Hashtag.findOne({ where: { title: query } });
-    let posts = [];
-    if (hashtag) {
-      posts = await hashtag.getPosts({ include: [{ model: User }] });
-    }
+// router.get('/hashtag', async (req, res, next) => {
+//   const query = req.query.hashtag;
+//   if (!query) {
+//     return res.redirect('/');
+//   }
+//   try {
+//     const hashtag = await Hashtag.findOne({ where: { title: query } });
+//     let posts = [];
+//     if (hashtag) {
+//       posts = await hashtag.getPosts({ include: [{ model: User }] });
+//     }
 
-    return res.render('nike', {
-      title: `${query} | 3e`,
-      twits: posts,
-    });
-  } catch (error) {
-    console.error(error);
-    return next(error);
-  }
-});
+//     return res.render('nike', {
+//       title: `${query} | 3e`,
+//       twits: posts,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return next(error);
+//   }
+// });
 
 module.exports = router;
