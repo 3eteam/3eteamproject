ddf877da593d3f8f9103f23cac6aa44fe838ec20
Seongjae <robbyl95@hanmail.net>
2021-10-06 14:10:12 +0900

    20211006_lsj_edit boardwrite.js(routes)

--- 3e/routes/boardwrite.js
+++ 3e/routes/boardwrite.js
@@ -1,18 +1,43 @@
 const express = require('express');
-
 const router  = express.Router();
+const { Comment, User } = require('../models');
 
+//boardwrite로 렌더링
 router.get('/',(req,res)=>{
     res.render('boardwrite');
 });
 
+
+
+router.get('/',async (req, res, next) => {
+  try {
+    const posts = await Comment.findAll({ 
+    include: {
+      model: User,
+      attributes: ['id'],
+    },
+    order: [['createdAt', 'DESC']],
+  });
+  res.render('boardwrite', {
+    title: '3e',
+    comment: posts,
+  });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+
+
+
 router.post('/', async (req, res, next) => {
     try {
+      console.log(req.user);
       const comment = await Comment.create({
-        id: req.body.id,
+        // id: req.body.id,
         comment: req.body.comment,
       });
-      console.log(comment);
       res.status(201).json(comment);
     } catch (err) {
       console.error(err);
