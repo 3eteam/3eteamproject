7932a9ccfd7c039e8ae7777c1d40163297700c4a
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-10-10 14:33:41 +0900

    add

--- 3e/routes/cart.js
+++ 3e/routes/cart.js
@@ -86,7 +86,43 @@ router.get('/hashtag', async (req, res, next) => {
   }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    const comment = await Comment.create({
+      commenter: req.body.id,
+      comment: req.body.comment,
+    });
+    console.log(comment);
+    res.status(201).json(comment);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
+router.route('/:id')
+  .patch(async (req, res, next) => {
+    try {
+      const result = await Comment.update({
+        comment: req.body.comment,
+      }, {
+        where: { id: req.params.id },
+      });
+      res.json(result);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const result = await Comment.destroy({ where: { id: req.params.id } });
+      res.json(result);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
 module.exports=router;
 
 
